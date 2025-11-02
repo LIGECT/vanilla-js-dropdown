17acf87dfec0f6452e2536d0596b0772fee82edb
LIGECT <viminiko@gmail.com>
2025-11-02 20:37:15 +0200

    refactor(dropdown): enhance existing HTML instead of generating it

--- src/main.js
+++ src/main.js
@@ -1,45 +1,40 @@
+console.log("Скрипт завелся");
 class Dropdown {
-  constructor(container, options) {
-    this.container = container;
-    this.options = options;
-    this.isOpen = false;
-    this.menuElement = null;
-    this.this.button = null;
-    this.render();
-  }
+  constructor(element) {
+    if (!element) {
+      throw new Error(
+        "Hey, you forgot to pass the element to the constructor!"
+      );
+    }
 
-  render() {
-    this.container.innerHTML = "";
-
-    this.button = document.createElement("button");
-    this.button.classList.add("dropdown-btn");
-    this.button.textContent = "menu";
-
-    this.menuElement = document.createElement("div");
-    this.menuElement.className = "dropdown-menu is-closed";
-
-    this.options.items.forEach((item) => {
-      const pointsMenu = document.createElement("div");
-      pointsMenu.textContent = item.text;
-      this.menuElement.appendChild(pointsMenu);
-    });
-
-    this.container.append(this.button, this.menuElement);
+    this.container = element;
+    this.toggleButton = this.container.querySelector(".dropdown-toggle");
+    this.menu = this.container.querySelector(".dropdown-menu");
 
     this.boundToggle = this.toggle.bind(this);
 
-    this.button.addEventListener("click", boundToggle);
+    if (!this.toggleButton || !this.menu) {
+      throw new Error(
+        "I can't find the button or menu. Check your HTML markup, you lazy bastard"
+      );
+    }
+
+    this.toggleButton.addEventListener("click", this.boundToggle);
   }
 
   toggle() {
-    this.menuElement.classList.toggle("is-closed");
-    this.isOpen = !this.isOpen;
+    this.menu.classList.toggle("is-closed");
   }
 
   destroy() {
-    this.button.removeEventListener("click", this.boundToggle);
+    this.toggleButton.removeEventListener("click", this.boundToggle);
     this.container.innerHTML = "";
   }
 }
+
+const allDropdowns = document.querySelectorAll(".dropdown");
+allDropdowns.forEach((dropdownElement) => {
+  new Dropdown(dropdownElement);
+});
 
 export default Dropdown;
